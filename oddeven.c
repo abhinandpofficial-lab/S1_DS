c67d093130eb3c9f691ed0bde5c9fd2522bbb2e7
abhinandpofficial-lab <abhinandpofficial@gmail.com>
2025-10-22 16:01:18 +0530

    Initial commit

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("the number is an even number");
+	}
+	else
+	{
+		printf("the number is an odd number");
+	}
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,sum;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number :");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("%d",sum);
+}
